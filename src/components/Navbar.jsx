9ac31a111fc21d6d078490a382749d02b289698c
Marco Salvoni <marco.salvoni8@gmail.com>
2023-11-22 17:38:56 +0100

    delete class in the svg favourite icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar () {
                 </li>
                 <li className="favourites-item">
                     <Link to="/favourites/">
-                        <svg id="favourite-icon" className="non-animated-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#1c263d" d="M11 2a3.944 3.944 0 00-3 1.371A3.944 3.944 0 005 2a4 4 0 00-2.865 6.785l5.508 5.629a.5.5 0 00.357.15.5.5 0 00.358-.151l5.315-5.454A3.988 3.988 0 0011 2z" class="0"></path></svg>
+                        <svg id="favourite-icon" className="non-animated-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#1c263d" d="M11 2a3.944 3.944 0 00-3 1.371A3.944 3.944 0 005 2a4 4 0 00-2.865 6.785l5.508 5.629a.5.5 0 00.357.15.5.5 0 00.358-.151l5.315-5.454A3.988 3.988 0 0011 2z"></path></svg>
                          Favourites
                     </Link>
                 </li>
